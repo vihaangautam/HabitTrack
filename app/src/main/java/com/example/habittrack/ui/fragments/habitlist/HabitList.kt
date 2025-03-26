4a7038982352231c3bbf184a812ba897c20bb184
Vihaan Gautam <vihaangautam04@gmail.com>
2025-03-26 20:29:51 +0530

    added onlickListener

--- app/src/main/java/com/example/habittrack/ui/fragments/habitlist/HabitList.kt
+++ app/src/main/java/com/example/habittrack/ui/fragments/habitlist/HabitList.kt
@@ -5,16 +5,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-
+import androidx.navigation.fragment.findNavController
 import com.example.habittrack.R
+import com.example.habittrack.databinding.FragmentHabitListBinding
 
-/**
- * A simple [Fragment] subclass.
- * Use the [HabitList.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HabitList : Fragment(R.layout.fragment_habit_list) {
+    private var _binding: FragmentHabitListBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHabitListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.fabAdd.setOnClickListener {
+            findNavController().navigate(R.id.action_habitList_to_createHabit)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }

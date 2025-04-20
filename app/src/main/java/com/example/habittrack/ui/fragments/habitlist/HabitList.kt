ea4e5e39a15199fca60c6cd8b961ad6bf1a4833a
Vihaan Gautam <vihaangautam04@gmail.com>
2025-04-21 04:31:09 +0530

    update

--- app/src/main/java/com/example/habittrack/ui/fragments/habitlist/HabitList.kt
+++ app/src/main/java/com/example/habittrack/ui/fragments/habitlist/HabitList.kt
@@ -1,15 +1,24 @@
 package com.example.habittrack.ui.fragments.habitlist
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.habittrack.R
+import com.example.habittrack.data.models.Habit
 import com.example.habittrack.databinding.FragmentHabitListBinding
+import com.example.habittrack.ui.fragments.habitlist.adapter.HabitListAdapter
+import com.example.habittrack.ui.viewmodels.HabitViewModel
 
 class HabitList : Fragment(R.layout.fragment_habit_list) {
+
+    private lateinit var habitList: List<Habit>
+    private lateinit var habitViewModel: HabitViewModel
+    private lateinit var adapter: HabitListAdapter
+
     private var _binding: FragmentHabitListBinding? = null
     private val binding get() = _binding!!
 
@@ -17,15 +26,55 @@ class HabitList : Fragment(R.layout.fragment_habit_list) {
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         _binding = FragmentHabitListBinding.inflate(inflater, container, false)
+        setHasOptionsMenu(true) // Set this here since it's called during fragment creation
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        adapter = HabitListAdapter()
+        binding.rvHabits.adapter = adapter
+        binding.rvHabits.layoutManager = LinearLayoutManager(context)
+
+        setupViewModel()
+
         binding.fabAdd.setOnClickListener {
             findNavController().navigate(R.id.action_habitList_to_createHabit)
         }
+
+        binding.swipeToRefresh.setOnRefreshListener {
+            adapter.setData(habitList)
+            binding.swipeToRefresh.isRefreshing = false
+        }
+    }
+
+    private fun setupViewModel() {
+        habitViewModel = ViewModelProvider(this).get(HabitViewModel::class.java)
+
+        habitViewModel.getAllHabits.observe(viewLifecycleOwner, Observer { habits ->
+            adapter.setData(habits)
+            habitList = habits
+
+            if (habits.isEmpty()) {
+                binding.rvHabits.visibility = View.GONE
+                binding.tvEmptyView.visibility = View.VISIBLE
+            } else {
+                binding.rvHabits.visibility = View.VISIBLE
+                binding.tvEmptyView.visibility = View.GONE
+            }
+        })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.nav_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_delete -> habitViewModel.deleteAllHabits()
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onDestroyView() {
